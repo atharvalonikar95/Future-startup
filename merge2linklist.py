7a4c8f93bb1cf8a044a040245711da00507d31a9
atharva lonikar <atharvalonikar95@gmail.com>
2024-01-09 23:39:07 +0530

    09-1-24

---
+++ merge2linklist.py
@@ -0,0 +1,44 @@
+list1=[]
+class ListNode:
+    def __init__(self,val,next=None) :
+        self.val=val
+        self.next=next
+
+class linkedlist:
+    def __init__(self,head=None):
+        self.head=head 
+    
+    def insert(self,val):
+        node=ListNode(val)
+        if self.head is None:
+            self.head=node
+            return
+        curr =self.head
+        while True:
+            if curr.next is None:
+                curr.next=node
+                break
+            curr=curr.next
+    def printlinkedlist(self):
+        curr=self.head
+        while curr is not None:
+            print(curr.val,"->",end=" ")
+            list1.append(curr.val)
+            list1.sort()
+            
+            curr=curr.next
+        print("none")
+
+
+ll1=linkedlist()
+ll1.insert(1)
+ll1.insert(5)
+ll1.printlinkedlist()
+
+ll2=linkedlist()
+ll2.insert(9)
+ll2.insert(4)
+ll2.printlinkedlist()
+print(list1)
+
+        
