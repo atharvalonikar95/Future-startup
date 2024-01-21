144a410c0a3446d39446edbd1a03a04c36789bf9
atharva lonikar <atharvalonikar95@gmail.com>
2024-01-22 03:12:48 +0530

    21-1-24

---
+++ rev only letters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        list1=[]
+        for i in s:
+            list1.append(i)
+        i, j = 0, len(list1) - 1
+
+        while i < j:
+            while i < j and not list1[i].isalpha():
+                i += 1
+            while i < j and not list1[j].isalpha():
+                j -= 1
+
+            if i < j:
+                temp = list1[i]
+                list1[i] = list1[j]
+                list1[j] = temp
+                i += 1
+                j -= 1
+
+        revStr = ''.join(list1)
+        return revStr
+Solution().reverseOnlyLetters()
