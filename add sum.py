4e6ee7c063efc80451a45f13c3657ea959c8113a
atharva lonikar <atharvalonikar95@gmail.com>
2024-01-28 23:53:49 +0530

    28-1-24

--- add sum.py
+++ add sum.py
@@ -1,20 +1,21 @@
-nums=[]
-def add(num):
-    
-    num=str(num)
-    
 
-    for i in num:
-        nums.append(int(i))
-    # print(nums)
-    # print(len(nums))
+class Solution:
+    
+    
+    def addDigits(self, num: int) -> int:
+        num = str(num)
+        nums=[]
 
-    sum=0
-    for i in range(len(nums)):
-        sum=sum+nums[i]
-    nums.clear()
-    # print(nums)
-    return sum
-print(add(32))
-if len(str(add(32)))>1:
-    print(add(add(32)))
+        for i in num:
+             nums.append(int(i))
+
+        sum=0
+        for i in range(len(nums)):
+            sum=sum+nums[i]
+
+        
+        if sum >= 10:
+            
+            return self.addDigits(sum)
+        else:
+            return sum
