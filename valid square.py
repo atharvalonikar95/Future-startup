a9d7423f13a78a683058c4ba9712389e88680857
atharva lonikar <atharvalonikar95@gmail.com>
2024-02-28 01:34:27 +0530

    28-02-24

---
+++ valid square.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        import math
+        sqr = math.sqrt(num)
+        sqr=str(sqr)
+
+        pt=sqr.index('.')+1
+        
+        if sqr[-1]=='0' :
+            return True
+        else:
+            return False
+
+        
+
+
+
+
+
+
+
+
+
+# num=681
+# import math
+# sqr=math.sqrt(num)
+# print(sqr)
+# print(type(sqr))
+# sr=str(sqr)
+
+# print(type(sr))
+
+# pt=sr.index('.')+1
+# print(pt)
+# print(sr[pt])
+# if sr[-1]=='0':
+#     print("true")
+# else:
+#     print("false")
