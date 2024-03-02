413a82ea34a903776b023ef83fcc7fccaa8d85c3
atharva lonikar <atharvalonikar95@gmail.com>
2024-03-02 15:49:35 +0530

    2-3-24

--- 3rdmax.py
+++ 3rdmax.py
@@ -1,36 +1,41 @@
-class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
+#            method 1
+
+# class Solution:
+#     def thirdMax(self, nums: List[int]) -> int:
    
-        nums=set(nums)
-        nums=list(nums)
+#         nums=set(nums)
+#         nums=list(nums)
         
-        nums.sort()
+#         nums.sort()
 
-        if len(nums)==2:
-            return nums[-1]
-        elif len(nums)==1:
-            return nums[-1]
-        elif len(nums)==0:
-            return []
-        else :
-            return nums[-3]
+#         if len(nums)==2:
+#             return nums[-1]
+#         elif len(nums)==1:
+#             return nums[-1]
+#         elif len(nums)==0:
+#             return []
+#         else :
+#             return nums[-3]
 
-        
+#            method 2
 
-# nums=[1,1,1]
+NUMS=[1,2,3]
 
-# print(type(nums))
-# nums=set(nums)
-# nums=list(nums)
-# print(type(nums))
-# nums.sort()
-# print(nums)
-# n= len(nums)
-# if n==2:
-#     print( nums[-2])
-# elif n==1:
-#     print( nums[-1])
-# elif n==0:
-#     print( [])
-# else :
-#     print( nums[-3])
+max1 = float('-inf')
+max2 = float('-inf')
+max3 = float('-inf')
+
+def thirdmax(NUMS,max1,max2,max3):
+    for i in range(len(NUMS)):
+        if NUMS[i]>=max1:
+            max3=max2
+            max2=max1
+            max1=NUMS[i]
+        if max2>NUMS[i]>max3 :
+            max2=NUMS[i]
+    return max3
+ans=thirdmax(NUMS,max1,max2,max3)
+print(ans)
+
+
+    
