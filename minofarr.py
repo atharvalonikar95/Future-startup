befbcc5b6c1b67424a2de3ee2ec38f61acd0df6e
atharva lonikar <atharvalonikar95@gmail.com>
2024-03-04 23:07:28 +0530

    23-07-24

---
+++ minofarr.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        nums.sort()
+        min1=float('+inf')
+        for i in range(len(nums)):
+            if nums[i]<=min1:
+                min1=nums[i]
+                break
+        return min1
