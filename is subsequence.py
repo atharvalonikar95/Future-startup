02c80e0122911ada10c24c280a9bfe9944b726a3
atharva lonikar <atharvalonikar95@gmail.com>
2024-03-12 23:55:03 +0530

    12-03-24

---
+++ is subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l=[]
+        if  s=="":
+            return True
+        s_idx=0
+        for i in t:
+            if i==s[s_idx]:
+                s_idx+=1
+                if s_idx == len(s):
+                    return True
+        return s_idx == len(s)
+
+
+        
+
+
+
+                
+        
